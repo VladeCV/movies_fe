854564c6ef727916c7a0aaa9ce8ed25b37ac9ccc
camilo.cabrera <camilo.cabrera@vicepresidencia.gob.bo>
2024-05-31 12:19:27 -0400

    List of items and styles

--- src/app/movie/movie.module.ts
+++ src/app/movie/movie.module.ts
@@ -4,6 +4,11 @@ import {CommonModule} from '@angular/common';
 import {MovieRoutingModule} from './movie-routing.module';
 import {MovieListComponent} from "./movie-list/movie-list.component";
 import {MovieFormComponent} from "./movie-form/movie-form.component";
+import {CardModule} from "primeng/card";
+import {ButtonModule} from "primeng/button";
+import {TableModule} from "primeng/table";
+import {ToolbarModule} from "primeng/toolbar";
+import {PanelModule} from "primeng/panel";
 
 
 @NgModule({
@@ -13,7 +18,12 @@ import {MovieFormComponent} from "./movie-form/movie-form.component";
   ],
   imports: [
     CommonModule,
-    MovieRoutingModule
+    MovieRoutingModule,
+    CardModule,
+    ButtonModule,
+    TableModule,
+    ToolbarModule,
+    PanelModule
   ]
 })
 export class MovieModule {
